d4d1df77e1183038f7b696da715e48aefdad802f
Moon Moon <purnsup90@gmail.com>
2023-03-03 01:06:16 -0700

    adf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React112
+          Learn React112 멋져
         </a>
       </header>
     </div>
